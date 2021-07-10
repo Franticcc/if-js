2df633d2e2e254cd8dd1a1bacf74803be3703e63
Franticcc <n.romanko.21vek.by@gmail.com>
2021-07-10 19:39:38 +0300

    main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+let  user = 'John Doe';
+console.log(user);
+let  student = 'Nikita';
+console.log(student);
+let  user = student;
+// the user variable assigns the value to the student variable, i.e. Nikita
+let  test = 1 + '1';
+// when adding a string data type to a numbered one, concatenation occurs - the strings are merged and you get 11
+test = test - 1 
+console.log(test);
+// test - = 1 short way
+// if we subtract one, then it turns out 10, since we have a number 11 with a string type, if we remove the string type, we polish 0
+let test = Boolean (1 + '1');
+ console.log(test);
+//  var test = !! 1 + '1'
+// will be true since we have a known number greater than zero occupying a string ...
