7bf020f8b56b870b8367c7d4ee0c5363e91d11bb
Franticcc <n.romanko.21vek.by@gmail.com>
2021-07-11 20:30:01 +0300

    JavaScript basics Part 2

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-let  user = 'John Doe';
-console.log(user);
-let  student = 'Nikita';
-console.log(student);
-let  user = student;
-// the user variable assigns the value to the student variable, i.e. Nikita
-let  test = 1 + '1';
-// when adding a string data type to a numbered one, concatenation occurs - the strings are merged and you get 11
-test = test - 1 
-console.log(test);
-// test - = 1 short way
-// if we subtract one, then it turns out 10, since we have a number 11 with a string type, if we remove the string type, we polish 0
-let test = Boolean (1 + '1');
- console.log(test);
-//  var test = !! 1 + '1'
-// will be true since we have a known number greater than zero occupying a string ...
+let array = [2, 3, 5, 8]
+
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
+
+let arrayMoreLess = [2, 5, 8, 15, 0, 6, 20, 3]
+
+for (let i = 0; i < arrayMoreLess.length; i++) {
+  if (arrayMoreLess[i] > 5 && arrayMoreLess[i] < 10)
+    console.log(arrayMoreLess[i]);
+}
+
+let arrayEvenElements = [2, 5, 8, 15, 0, 6, 20, 3]
+
+for (let i = 1; i < arrayEvenElements.length; i++) {
+  if (i % 2 !== 0) {
+    continue;
+  }
+  console.log(arrayEvenElements[i])
+}
