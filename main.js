3f00a11a57b0c26be61d3012b8239e73e7332361
Franticcc <n.romanko.21vek.by@gmail.com>
2021-07-12 22:32:11 +0300

    work on bugs lesson-2-2

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
-let array = [2, 3, 5, 8]
+const array = [2, 3, 5, 8]
+let sum = 1;
 
 for (let i = 0; i < array.length; i++) {
-  console.log(array[i]);
-}
+  sum *= array[i]
+};
 
-let arrayMoreLess = [2, 5, 8, 15, 0, 6, 20, 3]
+console.log(sum);
+
+
+const arrayMoreLess = [2, 5, 8, 15, 0, 6, 20, 3]
 
 for (let i = 0; i < arrayMoreLess.length; i++) {
   if (arrayMoreLess[i] > 5 && arrayMoreLess[i] < 10)
     console.log(arrayMoreLess[i]);
-}
+};
 
-let arrayEvenElements = [2, 5, 8, 15, 0, 6, 20, 3]
+const arrayEvenElements = [2, 5, 8, 15, 0, 6, 20, 3]
 
-for (let i = 1; i < arrayEvenElements.length; i++) {
-  if (i % 2 !== 0) {
-    continue;
-  }
+for (let i = 0; i < arrayEvenElements.length; i++) {
+  if (i % 2) 
   console.log(arrayEvenElements[i])
-}
+};
+
