67201539ec837bd06baa7b4e3817db73576b6cd7
Franticcc <n.romanko.21vek.by@gmail.com>
2021-07-13 00:05:50 +0300

    work on bugs-2 lesson-2-2

--- main.js
+++ main.js
@@ -12,13 +12,13 @@ const arrayMoreLess = [2, 5, 8, 15, 0, 6, 20, 3]
 
 for (let i = 0; i < arrayMoreLess.length; i++) {
   if (arrayMoreLess[i] > 5 && arrayMoreLess[i] < 10)
-    console.log(arrayMoreLess[i]);
+    console.log(arrayMoreLess);
 };
 
 const arrayEvenElements = [2, 5, 8, 15, 0, 6, 20, 3]
 
 for (let i = 0; i < arrayEvenElements.length; i++) {
-  if (i % 2) 
+  if (arrayEvenElements[i] % 2 === 0)
   console.log(arrayEvenElements[i])
 };
 
