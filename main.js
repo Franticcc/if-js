8dfc2fbe328a5eff3c52ed1a45b849f6ee7e1f52
NikitaRomanko <83835203+Franticcc@users.noreply.github.com>
2021-07-14 09:48:40 +0300

    Lesson 2 2 (#3)
    * main.js
    
    * JavaScript basics Part 2
    
    * work on bugs lesson-2-2
    
    * work on bugs-2 lesson-2-2
    
    * fixed loop error
    
    * work on bugs-3 lesson-2-2

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-let  user = 'John Doe';
-console.log(user);
-let  student = 'Nikita';
-console.log(student);
-let  user = student;
-// the user variable assigns the value to the student variable, i.e. Nikita
-let  test = 1 + '1';
-// when adding a string data type to a numbered one, concatenation occurs - the strings are merged and you get 11
-test = test - 1 
-console.log(test);
-// test - = 1 short way
-// if we subtract one, then it turns out 10, since we have a number 11 with a string type, if we remove the string type, we polish 0
-let test = Boolean (1 + '1');
- console.log(test);
-//  var test = !! 1 + '1'
-// will be true since we have a known number greater than zero occupying a string ...
+const array = [2, 3, 5, 8];
+let sum = 1;
+
+for (let i = 0; i < array.length; i++) {
+  sum *= array[i];
+}
+
+console.log(sum);
+
+const arrayMoreLess = [2, 5, 8, 15, 0, 6, 20, 3];
+
+for (let i = 0; i < arrayMoreLess.length; i++) {
+  if (arrayMoreLess[i] > 5 && arrayMoreLess[i] < 10) {
+    console.log(arrayMoreLess[i]);
+  }
+}
+
+const arrayEvenElements = [2, 5, 8, 15, 0, 6, 20, 3];
+
+for (let i = 0; i < arrayEvenElements.length; i++) {
+  if (arrayEvenElements[i] % 2 === 0) {
+    console.log(arrayEvenElements[i]);
+  } 
+}
