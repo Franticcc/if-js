a4d431646be1b9b0d3f53c47ab86dd064e71769f
Franticcc <n.romanko.21vek.by@gmail.com>
2021-07-21 21:30:52 +0300

    Function part 2

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
-let  user = 'John Doe';
-console.log(user);
-let  student = 'Nikita';
-console.log(student);
-let  user = student;
-// the user variable assigns the value to the student variable, i.e. Nikita
-let  test = 1 + '1';
-// when adding a string data type to a numbered one, concatenation occurs - the strings are merged and you get 11
-test = test - 1 
+function checkPalindrome(str) {
+  const strRevers = str.split('').reverse().join('');
+  if (strRevers === str) {
+    return true;
+  } 
+    return false;
+}
+
+test = checkPalindrome('abcdedcba');
+test2 = checkPalindrome('sfsfgaa');
+
 console.log(test);
-// test - = 1 short way
-// if we subtract one, then it turns out 10, since we have a number 11 with a string type, if we remove the string type, we polish 0
-let test = Boolean (1 + '1');
- console.log(test);
-//  var test = !! 1 + '1'
-// will be true since we have a known number greater than zero occupying a string ...
+
+function min(a, b) {
+  if (a < b) {
+    return a;
+  }
+  return b;
+}
+
+console.log(min(3, 5));
+
+function max(a, y) {
+  if (y > a) {
+    return y;
+  }
+  return a;
+}
+console.log(max(10, 15));
+
+const zeroArray = [10, 15, 100, 525, 300, 200, 42, 54, 60, 560];
+const sumZeroArray = (zeroArray) => {
+  let result = zeroArray.map((item) => {
+     return (item % 10 === 0) ? item.toString().replace(/0/g, 'zero') : item;
+  });
+  return result;
+}
+
+console.log(sumZeroArray(zeroArray));
+
