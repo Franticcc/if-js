a77d29af446ddaf6fc13512e9c1fb971afa75303
Franticcc <n.romanko.21vek.by@gmail.com>
2021-08-04 14:56:49 +0300

    string and arraw

--- main.js
+++ main.js
@@ -1,27 +1,63 @@
-// const sum = (a) => (b) => a + b;
-function sum(a) {
-  return (b) => a + b;
+const date = '2020-11-26';
+
+const dateСhange = (date) => date.split('-').reverse().join('.');
+
+console.log(dateСhange(date));
+
+const arr = [
+  {
+    country: 'Russia',
+    city: 'Saint Petersburg',
+    hotel: 'Hotel Leopold',
+  },
+  {
+    country: 'Spain',
+    city: 'Santa Cruz de Tenerife',
+    hotel: 'Apartment Sunshine',
+  },
+  {
+    country: 'Slowakia',
+    city: 'Vysokie Tatry',
+    hotel: 'Villa Kunerad',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hostel Friendship',
+  },
+  {
+    country: 'Indonesia',
+    city: 'Bali',
+    hotel: 'Ubud Bali Resort&SPA',
+  },
+  {
+    country: 'Netherlands',
+    city: 'Rotterdam',
+    hotel: 'King Kong Hostel',
+  },
+  {
+    country: 'Marocco',
+    city: 'Ourika',
+    hotel: 'Rokoko Hotel',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hotel Rehberge Berlin Mitte',
+  },
+];
+
+const faindPlace = (placeName) => {
+  let = result = '';
+  const regexp = new RegExp(placeName, 'img');
+  
+  arr.forEach((placeInfo) => {
+    if(regexp.test(placeInfo.country + placeInfo.city + placeInfo.hotel)) {
+      result = `country: ${placeInfo.country}, city: ${placeInfo.city}, hotel: ${placeInfo.hotel}`;
+    }
+  });
+
+  return result || 'info not found ';
 }
 
-console.log(sum(5)(2));
-
-
-const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-const text = document.getElementsByTagName('p');
-// const text = document.getElementById('text');- I can't do it through id
-
-const changeTextColor = () => {
-	let color = 0; 
-	return function () {
-		this.style.color = colors[color];
-		color++;
-    
-		if (color === colors.length) {
-      color = 0;
-    };
-	};
-};
-
-for (let i = 0; i < text.length; i++) {
-	text[i].addEventListener('click', changeTextColor());
-};
+console.log(faindPlace('Germany'));
