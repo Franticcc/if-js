add6ef07ddca5ca0e44dda8a121a38b1ea0fcb5e
Franticcc <n.romanko.21vek.by@gmail.com>
2021-08-12 21:36:56 +0300

    error correction lesson-5

--- main.js
+++ main.js
@@ -8,15 +8,13 @@ console.log(sum(5)(2));
 
 const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
 const text = document.getElementsByTagName('p');
-// const text = document.getElementById('text');- I can't do it through id
 
 const changeTextColor = () => {
 	let color = 0; 
 	return function () {
 		this.style.color = colors[color];
 		color++;
-    
-		if (color === colors.length) {
+  	  if (color === colors.length) {
       color = 0;
     };
 	};
@@ -25,3 +23,25 @@ const changeTextColor = () => {
 for (let i = 0; i < text.length; i++) {
 	text[i].addEventListener('click', changeTextColor());
 };
+
+
+// const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+// const text1 = document.getElementsById('text1');
+// const text2 = document.getElementsById('text2');
+// const text3 = document.getElementsById('text3');
+// // const text = document.getElementById('text');- I can't do it through id
+
+// const changeTextColor = (textElement) => {
+// 	let color = 0;
+// 	return function () {
+// 		textElement.style.color = colors[color];
+// 		color++;
+// 		if (color === colors.length) {
+// 			color = 0;
+// 		};
+// 	};
+// };
+
+// text1.addEventListener('click', changeTextColor(text1));
+// text2.addEventListener('click', changeTextColor(text2));
+// text3.addEventListener('click', changeTextColor(text3));
