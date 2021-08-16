dae6359795f7ba0a9bbea70485576328535a73e4
Franticcc <n.romanko.21vek.by@gmail.com>
2021-08-16 15:19:39 +0300

    lesson-8 Deep Object Equality

--- main.js
+++ main.js
@@ -336,3 +336,64 @@ const arrayWorld = (arrayCountry) => {
 }, {});
 }
 console.log(arrayWorld(hotels));
+
+const obj1 = {
+	a: 'a',
+	b: {
+		a: 'a',
+		b: 'b',
+		c: {
+			a: 1,
+		},
+	},
+};
+const obj2 = {
+	b: {
+		c: {
+			a: 1,
+		},
+		b: 'b',
+		a: 'a',
+	},
+	a: 'a',
+};
+const obj3 = {
+	a: {
+		c: {
+			a: 'a',
+		},
+		b: 'b',
+		a: 'a',
+	},
+	b: 'b',
+};
+
+const deepEqual = (object1, object2) => {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    const val1 = object1[key];
+    const val2 = object2[key];
+    const areObjects = isObject(val1) && isObject(val2);
+    if (
+      areObjects && !deepEqual(val1, val2) ||
+      !areObjects && val1 !== val2
+    ) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function isObject(object) {
+  return object != null && typeof object === 'object';
+};
+
+console.log(deepEqual(obj1, obj2));
+console.log(deepEqual(obj1, obj3));
