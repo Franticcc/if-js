62afb2b02086f1735092e75224f4b522b7ab8148
Franticcc <n.romanko.21vek.by@gmail.com>
2021-08-22 21:16:27 +0300

    constructor

--- main.js
+++ main.js
@@ -1,399 +1,100 @@
-const palindrome = 'civic';
-
-const palindromeСhange = (palindrome) => palindrome === palindrome.split('').reverse().join('');
-
-console.log(palindromeСhange(palindrome));
-
-const data = [
+const studentsData = [
   {
-    name: 'Hotel Leopold',
-    city: 'Saint Petersburg',
-    country: 'Russia',
+    firstName: 'Василий',
+    lastName: 'Петров',
+    admissionYear: 2019,
+    courseName: 'Java',
   },
   {
-    name: 'Apartment Sunshine',
-    city: 'Santa Cruz de Tenerife',
-    country: 'Spain',
+    firstName: 'Иван',
+    lastName: 'Иванов',
+    admissionYear: 2018,
+    courseName: 'JavaScript',
   },
   {
-    name: 'Villa Kunerad',
-    city: 'Vysokie Tatry',
-    country: 'Slowakia',
+    firstName: 'Александр',
+    lastName: 'Федоров',
+    admissionYear: 2017,
+    courseName: 'Python',
   },
   {
-    name: 'Hostel Friendship',
-    city: 'Berlin',
-    country: 'Germany',
-  },
-  {
-    name: 'Radisson Blu Hotel',
-    city: 'Kyiv',
-    country: 'Ukraine',
-  },
-  {
-    name: 'Paradise Hotel',
-    city: 'Guadalupe',
-    country: 'Mexico',
-  },
-  {
-    name: 'Hotel Grindewald',
-    city: 'Interlaken',
-    country: 'Switzerland',
-  },
-  {
-    name: 'The Andaman Resort',
-    city: 'Port Dickson',
-    country: 'Malaysia',
-  },
-  {
-    name: 'Virgin Hotel',
-    city: 'Chicago',
-    country: 'USA',
-  },
-  {
-    name: 'Grand Beach Resort',
-    city: 'Dubai',
-    country: 'United Arab Emirates',
-  },
-  {
-    name: 'Shilla Stay',
-    city: 'Seoul',
-    country: 'South Korea',
-  },
-  {
-    name: 'San Firenze Suites',
-    city: 'Florence',
-    country: 'Italy',
-  },
-  {
-    name: 'The Andaman Resort',
-    city: 'Port Dickson',
-    country: 'Malaysia',
-  },
-  {
-    name: 'Black Penny Villas',
-    city: 'BTDC, Nuca Dua',
-    country: 'Indonesia',
-  },
-  {
-    name: 'Koko Hotel',
-    city: 'Tokyo',
-    country: 'Japan',
-  },
-  {
-    name: 'Ramada Plaza',
-    city: 'Istanbul',
-    country: 'Turkey',
-  },
-  {
-    name: 'Waikiki Resort Hotel',
-    city: 'Hawaii',
-    country: 'USA',
-  },
-  {
-    name: 'Puro Hotel',
-    city: 'Krakow',
-    country: 'Poland',
-  },
-  {
-    name: 'Asma Suites',
-    city: 'Santorini',
-    country: 'Greece',
-  },
-  {
-    name: 'Cityden Apartments',
-    city: 'Amsterdam',
-    country: 'Netherlands',
-  },
-  {
-    name: 'Mandarin Oriental',
-    city: 'Miami',
-    country: 'USA',
-  },
-  {
-    name: 'Concept Terrace Hotel',
-    city: 'Rome',
-    country: 'Italy',
-  },
-  {
-    name: 'Ponta Mar Hotel',
-    city: 'Fortaleza',
-    country: 'Brazil',
-  },
-  {
-    name: 'Four Seasons Hotel',
-    city: 'Sydney',
-    country: 'Australia',
-  },
-  {
-    name: 'Le Meridien',
-    city: 'Nice',
-    country: 'France',
-  },
-  {
-    name: 'Apart Neptun',
-    city: 'Gdansk',
-    country: 'Poland',
-  },
-  {
-    name: 'Lux Isla',
-    city: 'Ibiza',
-    country: 'Spain',
-  },
-  {
-    name: 'Nox Hostel',
-    city: 'London',
-    country: 'UK',
-  },
-  {
-    name: 'Leonardo Vienna',
-    city: 'Vienna',
-    country: 'Austria',
-  },
-  {
-    name: 'Adagio Aparthotel',
-    city: 'Edinburgh',
-    country: 'UK',
-  },
-  {
-    name: 'Steigenberger Hotel',
-    city: 'Hamburg',
-    country: 'Germany',
-  },
+    firstName: 'Николай',
+    lastName: 'Петров',
+    admissionYear: 2019,
+    courseName: 'Android',
+  }
 ];
 
-const faindPlace = (placeName, regexp) => {
- regexp = new RegExp(regexp, 'img');
- return placeName.reduce((obj, element) => regexp.test(Object.values(element)) ? obj + `${Object.values(element).reverse()} ` : obj, []);
-}
-
-console.log(faindPlace(data, 'USA')); 
-
-const hotels = [
-  {
-    name: 'Hotel Leopold',
-    city: 'Saint Petersburg',
-    country: 'Russia',
-  },
-  {
-    name: 'Apartment Sunshine',
-    city: 'Santa Cruz de Tenerife',
-    country: 'Spain',
-  },
-  {
-    name: 'Villa Kunerad',
-    city: 'Vysokie Tatry',
-    country: 'Slowakia',
-  },
-  {
-    name: 'Hostel Friendship',
-    city: 'Berlin',
-    country: 'Germany',
-  },
-  {
-    name: 'Radisson Blu Hotel',
-    city: 'Kyiv',
-    country: 'Ukraine',
-  },
-  {
-    name: 'Paradise Hotel',
-    city: 'Guadalupe',
-    country: 'Mexico',
-  },
-  {
-    name: 'Hotel Grindewald',
-    city: 'Interlaken',
-    country: 'Switzerland',
-  },
-  {
-    name: 'The Andaman Resort',
-    city: 'Port Dickson',
-    country: 'Malaysia',
-  },
-  {
-    name: 'Virgin Hotel',
-    city: 'Chicago',
-    country: 'USA',
-  },
-  {
-    name: 'Grand Beach Resort',
-    city: 'Dubai',
-    country: 'United Arab Emirates',
-  },
-  {
-    name: 'Shilla Stay',
-    city: 'Seoul',
-    country: 'South Korea',
-  },
-  {
-    name: 'San Firenze Suites',
-    city: 'Florence',
-    country: 'Italy',
-  },
-  {
-    name: 'The Andaman Resort',
-    city: 'Port Dickson',
-    country: 'Malaysia',
-  },
-  {
-    name: 'Black Penny Villas',
-    city: 'BTDC, Nuca Dua',
-    country: 'Indonesia',
-  },
-  {
-    name: 'Koko Hotel',
-    city: 'Tokyo',
-    country: 'Japan',
-  },
-  {
-    name: 'Ramada Plaza',
-    city: 'Istanbul',
-    country: 'Turkey',
-  },
-  {
-    name: 'Waikiki Resort Hotel',
-    city: 'Hawaii',
-    country: 'USA',
-  },
-  {
-    name: 'Puro Hotel',
-    city: 'Krakow',
-    country: 'Poland',
-  },
-  {
-    name: 'Asma Suites',
-    city: 'Santorini',
-    country: 'Greece',
-  },
-  {
-    name: 'Cityden Apartments',
-    city: 'Amsterdam',
-    country: 'Netherlands',
-  },
-  {
-    name: 'Mandarin Oriental',
-    city: 'Miami',
-    country: 'USA',
-  },
-  {
-    name: 'Concept Terrace Hotel',
-    city: 'Rome',
-    country: 'Italy',
-  },
-  {
-    name: 'Ponta Mar Hotel',
-    city: 'Fortaleza',
-    country: 'Brazil',
-  },
-  {
-    name: 'Four Seasons Hotel',
-    city: 'Sydney',
-    country: 'Australia',
-  },
-  {
-    name: 'Le Meridien',
-    city: 'Nice',
-    country: 'France',
-  },
-  {
-    name: 'Apart Neptun',
-    city: 'Gdansk',
-    country: 'Poland',
-  },
-  {
-    name: 'Lux Isla',
-    city: 'Ibiza',
-    country: 'Spain',
-  },
-  {
-    name: 'Nox Hostel',
-    city: 'London',
-    country: 'UK',
-  },
-  {
-    name: 'Leonardo Vienna',
-    city: 'Vienna',
-    country: 'Austria',
-  },
-  {
-    name: 'Adagio Aparthotel',
-    city: 'Edinburgh',
-    country: 'UK',
-  },
-  {
-    name: 'Steigenberger Hotel',
-    city: 'Hamburg',
-    country: 'Germany',
-  },
-];
-
-const arrayWorld = (arrayCountry) => {
-  return arrayCountry.reduce((listCountryCity, element) => {
-    Object.keys(listCountryCity).includes(element.country) 
-      ? listCountryCity[`${element.country}`].push(element.city) 
-      : listCountryCity[`${element.country}`] = [element.city];
-    return listCountryCity;
-}, {});
-}
-console.log(arrayWorld(hotels));
-
-const obj1 = {
-	a: 'a',
-	b: {
-		a: 'a',
-		b: 'b',
-		c: {
-			a: 1,
-		},
-	},
-};
-const obj2 = {
-	b: {
-		c: {
-			a: 1,
-		},
-		b: 'b',
-		a: 'a',
-	},
-	a: 'a',
-};
-const obj3 = {
-	a: {
-		c: {
-			a: 'a',
-		},
-		b: 'b',
-		a: 'a',
-	},
-	b: 'b',
-};
-
-const deepEqual = (object1, object2) => {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
-
-  if (keys1.length !== keys2.length) {
-    return false;
+class User {
+  constructor( parameter ) {
+    this.firstName = parameter.firstName;
+    this.lastName = parameter.lastName;
   }
 
-  for (const key of keys1) {
-    const val1 = object1[key];
-    const val2 = object2[key];
-    const areObjects = isObject(val1) && isObject(val2);
-    if (
-      areObjects && !deepEqual(val1, val2) ||
-      !areObjects && val1 !== val2
-    ) {
-      return false;
-    }
+  get fullName() {
+    return this.firstName + ' ' + this.lastName;
   }
-
-  return true;
 }
 
-function isObject(object) {
-  return object != null && typeof object === 'object';
-};
+class Student extends User {
+  constructor(parameter ) {
+    super(parameter);
+    this.admissionYear = parameter.admissionYear;
+    this.courseName = parameter.courseName;
+  }
 
-console.log(deepEqual(obj1, obj2));
-console.log(deepEqual(obj1, obj3));
+  get course() { 
+    return new Date().getFullYear() - this.admissionYear; 
+   }
+}
+
+class Students {
+  constructor(studentsData) {
+    this.studentsData = studentsData; 
+  }
+
+  getInfo() {
+    return this.studentsData.map((infoStudent) => 
+    `${new User(infoStudent).fullName} - ${new Student(infoStudent).courseName} - ${new Student(infoStudent).course} курс`,);
+  }
+}
+
+const students = new Students(studentsData);
+console.log(students.getInfo());
+
+// class User {
+//     constructor( firstName, lastName ) {
+//       this.firstName = firstName;
+//       this.lastName = lastName;
+//     }
+  
+//     get fullName() {
+//       return this.firstName + ' ' + this.lastName;
+//     }
+//   }
+  
+//   class Student extends User {
+//     constructor(firstName, lastName, admissionYear, courseName ) {
+//       super(firstName, lastName );
+//       this.admissionYear = admissionYear;
+//       this.courseName = courseName;
+//     }
+  
+//     get course() { 
+//       return new Date().getFullYear() - this.admissionYear; 
+//      }
+//   }
+  
+//   class Students {
+//     constructor(studentsData) {
+//       this.studentsData = studentsData; 
+//     }
+  
+//     getInfo() {
+//       return this.studentsData.map((infoStudent) => 
+//       `${new User(infoStudent).fullName} - ${new Student(infoStudent).courseName} - ${new Student(infoStudent).course} курс`,);
+//     }
+//   }
+  
+//   const students = new Students(studentsData);
+//   console.log(students.getInfo());
